de2f92d60cb5eb960b63ff8ec6c7cdb62860f4bd
unknown <huangxuxin@eainc.com>
2018-01-31 18:21:33 +0800

    add server test

--- js/learningSocket/ss-local.js
+++ js/learningSocket/ss-local.js
@@ -1,5 +1,7 @@
 const net = require("net");
+const colors = require('colors')
 
+//LocalSockt Socket接口 用于像ss-Server 发送消息
 var LocalSocket = new net.Socket();
 
 LocalSocket.connect({
@@ -7,34 +9,42 @@ LocalSocket.connect({
     port:10086
 })
 
+//LocalServer 本地服务器，用于监听本地端口消息，并进行转发
 var LocalServer = net.createServer((conn)=>{
-    console.log('client connected')
+
+    //conn为客户机的链接向本地服务器的socket链接
+    console.log('client connected'.green)
+
+    //向本地端口发送一个连接成功的消息
+    //conn.write('LocalServer connected success')
+
+    //当链接断开的时候，打印一个log    
     conn.on('end',function(){
-        console.log('client disconnected')
-    })
-    conn.write('hello')
-    conn.on('error',function(e){
+        console.log('client disconnected'.red)
+    }).on('error',function(e){
+        console.log('conn error')
         console.log(e)
-    })
-    conn.on('close',function(e){
-        console.log(e)
-    })
-    conn.on('data',function(data){
-        console.log(data)
+    }).on('close',function(){
+        console.log('conn close')
+    }).on('data',function(data){
+        //监听消息
+        console.log('localServer getMessage'.green);
+        console.log(data.toString('utf-8'));
+        //当有消息进入的时候，将消息转发至ssServer
         LocalSocket.write(data)
     })
-    conn.pipe(conn)
 })
 
+//LocalServer 监听异常出错
 LocalServer.on('error',function(err){
     throw err;
 })
 
-LocalServer.listen(1081,function(){
-    console.log('the local Server is listening')
-})
 
 LocalServer.on('close',function(){
     console.log('localsocket close');
 })
 
+LocalServer.listen(1081,function(){
+    console.log('the local Server is listening')
+})
--- js/learningSocket/ss-server.js
+++ js/learningSocket/ss-server.js
@@ -3,20 +3,19 @@ const net = require("net"),
 
 //接收从localServer传来的消息并转发给目标服务器
 var ssServer = net.createServer((conn)=>{
-    console.log('sslocal connected')
+    console.log('sslocal connected'.bgGreen)
     conn.on('end',function(){
-        console.log('sslocal disconnected')
+        console.log('sslocal disconnected'.green)
     })
     conn.on('data',function(data){
-        console.log('########'.green)
-        console.log(data)
-        console.log(data.toString('utf-8').blue)
+        console.log('ss Server get message'.green)
+        console.log(data.toString('utf-8'))
     })
     conn.on('error',function(e){
-        console.log(e)
+        console.log(e.red)
     })
     conn.on('close',function(e){
-        console.log(e)
+        console.log(e.red)
     })
 })
 
@@ -25,9 +24,9 @@ ssServer.on('error',function(err){
 })
 
 ssServer.listen(10086,function(){
-    console.log('the lightSocket Server is listening')
+    console.log('the lightSocket Server is listening'.green)
 })
 
 ssServer.on('close',function(){
-    console.log('ssServer close');
+    console.log('ssServer close'.red);
 })
